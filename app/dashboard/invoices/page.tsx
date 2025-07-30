e8bf8b58ac5aed6dc5a0d41b6831ae73227a455c
dandersonstack <danieltanders@gmail.com>
2025-07-30 11:09:36 -0700

    feat: up to chapter 14, but with invoice and invoices seperated

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -18,7 +18,7 @@ export default async function Page(props: {
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
     const totalPages = await fetchInvoicesPages(query);
-  
+
 
   return (
     <div className="w-full">
